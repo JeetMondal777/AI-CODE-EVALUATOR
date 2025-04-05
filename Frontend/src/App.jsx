b99b1074ea94cfbe7c008febcde84d1c141fd8b5
Jeet Mondal <jeetmn0226@gmail.com>
2025-04-05 11:51:05 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,12 +2,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/components/LandingPage";
+import QuestionPage from "./pages/components/questionPage";
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/question" element={<QuestionPage />} />
       </Routes>
     </Router>
   );
